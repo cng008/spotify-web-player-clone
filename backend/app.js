1a2c775371594f1de4bd4ef2affb953b20d635a0
Christien Ng <ngchristien@gmail.com>
2022-09-02 17:20:42 -0700

    set up backend env

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+/** Express backend for spotify_clone. */
+
+const express = require('express');
+
+const { NotFoundError } = require('./expressError');
+
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== 'test') console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status }
+  });
+});
+
+module.exports = app;
