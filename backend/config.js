cddfd053d139c34d69a0f5d9735d847c8982b56a
Christien Ng <ngchristien@gmail.com>
2022-09-25 23:08:34 -0700

    ready to deploy backend

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,7 @@
 /** Shared config for application; can be required many places. */
 
 require('dotenv').config();
+// require('colors');
 
 const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 
